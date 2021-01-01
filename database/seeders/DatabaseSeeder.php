3dd086dc62b1131b74c4593f9c9e6a4d5cee0d8b
PhucDuong-SSS <phucdnDev@gmail.com>
2021-01-01 22:19:25 +0700

    make it better122d

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $user= new User();
-        $user->name ='phuc';
-        $user->username ='phuc';
-        $user->email ='phuc@gmail.com';
-        $user->password =bcrypt('123456');
+        $user->name ='admin';
+        $user->username ='admin';
+        $user->email ='admin@gmail.com';
+        $user->phone ='0975396865';
+        $user->category =1;
+        $user->coupon =1;
+        $user->blog =1;
+        $user->order =1;
+        $user->other =1;
+        $user->report =1;
+        $user->role =1;
+        $user->return =1;
+        $user->contact =1;
+        $user->comment =1;
+        $user->setting =1;
+        $user->stock =1;
+        $user->type =1;
         $user->save();
     }
 }
