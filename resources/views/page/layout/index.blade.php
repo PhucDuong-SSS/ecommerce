de620b8a5570e45b1ffccf85a9e2db77f2e28b5b
PhucDuong-SSS <phucdnDev@gmail.com>
2021-01-02 09:46:42 +0700

    fix row

--- resources/views/page/layout/index.blade.php
+++ resources/views/page/layout/index.blade.php
@@ -1,7 +1,7 @@
 @extends('page.layout.app_layout')
 @section('css')
-        <link rel="stylesheet" type="text/css" href="{{ asset('frontend/styles/blog_styles.css') }}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('frontend/styles/blog_responsive.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/styles/blog_styles.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/styles/blog_responsive.css') }}">
 
 @endsection
 @section('script')
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="slide">
                 <div class="sidebar">
-                        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
                         <ol class="carousel-indicators">
                             <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
 
@@ -41,11 +41,13 @@
                             </div>
 
                         </div>
-                        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+                        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button"
+                           data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                         </a>
-                        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+                        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button"
+                           data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </a>
@@ -63,133 +65,143 @@
             <div class="row">
                 @foreach($categories as $category)
 
-                <div class="col-12">
+                    <div class="col-12">
 
-                    <div class="product-wap d-flex flex-row align-items-start justify-content-between">
-                        <!-- navbar title-->
-                        <div class="block-product__menu d-flex flex-row ">
-                            <h2 class="block-product__menu-title"><a href="{{route('product.showProductCategory',['id'=>$category->id])}}">{{$category->name}}</a></h2>
-                            <ul class="d-flex flex-row">
-                                @foreach($category->sub_categories as $subCate)
-                                <li><a class="product-link" href="{{route('product.showProductSubCategory',['id'=>$subCate->id])}}">{{$subCate->name}}</a></li>
+                        <div class="product-wap d-flex flex-row align-items-start justify-content-between">
+                            <!-- navbar title-->
+                            <div class="block-product__menu d-flex flex-row ">
+                                <h2 class="block-product__menu-title"><a
+                                        href="{{route('product.showProductCategory',['id'=>$category->id])}}">{{$category->name}}</a>
+                                </h2>
+                                <ul class="d-flex flex-row">
+                                    @foreach($category->sub_categories as $subCate)
+                                        <li><a class="product-link"
+                                               href="{{route('product.showProductSubCategory',['id'=>$subCate->id])}}">{{$subCate->name}}</a>
+                                        </li>
 
-                                @endforeach
+                                    @endforeach
 
-                            </ul>
+                                </ul>
+                            </div>
+
                         </div>
-
                     </div>
-                </div>
 
 
-           <div class="row ">
-            @foreach($category->products->take(8) as $product)
-                <div class="col-md-3 col-sm-6">
-                    <div class="product-grid4">
-                        <div class="product-image4">
-                            <a href="{{route('product.showDetails',['id'=>$product->id])}}">
-                                <img class="pic-1" src="{{$product->getUrl().$product->image_one}}">
-                                <img class="pic-2" src="{{$product->getUrl().$product->image_two}}">
-                            </a>
-                            <ul class="social">
-                                <li><a href="{{route('product.showDetails',['id'=>$product->id])}}" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
-    {{--                            <li><a  href="#" data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>--}}
-                                <li><a class="addcart" data-id="{{$product->id}}" href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
-                            </ul>
-                            @if($product->discount_price == NULL)
 
-                            <span class="product-new-label">New</span>
-                            @else
-                            <span class="product-discount-label">
+                        @foreach($category->products->take(8) as $product)
+                            <div class="col-md-3 col-sm-6">
+                                <div class="product-grid4">
+                                    <div class="product-image4">
+                                        <a href="{{route('product.showDetails',['id'=>$product->id])}}">
+                                            <img class="pic-1" src="{{$product->getUrl().$product->image_one}}">
+                                            <img class="pic-2" src="{{$product->getUrl().$product->image_two}}">
+                                        </a>
+                                        <ul class="social">
+                                            <li><a href="{{route('product.showDetails',['id'=>$product->id])}}"
+                                                   data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                                            {{--                            <li><a  href="#" data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>--}}
+                                            <li><a class="addcart" data-id="{{$product->id}}" href="#"
+                                                   data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                                        </ul>
+                                        @if($product->discount_price == NULL)
+
+                                            <span class="product-new-label">New</span>
+                                        @else
+                                            <span class="product-discount-label">
                                   @php
                                       $amount = $product->selling_price - $product->discount_price;
                                       $discount = $amount/$product->selling_price*100;
                                   @endphp
 
-                                {{ intval($discount) }}%
+                                                {{ intval($discount) }}%
                             </span>
-                            @endif
-                        </div>
-                        <div class="product-content">
-                            <h3 class="title"><a href="#">{{$product->name}}</a></h3>
-                            <div class="price">
-                                @if($product->discount_price == NULL)
-                                    ${{ $product->selling_price }}
-                                @else
-                                    ${{$product->discount_price}}
-                                <span>${{$product->selling_price}}</span>
-                                    @endif
+                                        @endif
+                                    </div>
+                                    <div class="product-content">
+                                        <h3 class="title"><a href="#">{{$product->name}}</a></h3>
+                                        <div class="price">
+                                            @if($product->discount_price == NULL)
+                                                ${{ $product->selling_price }}
+                                            @else
+                                                ${{$product->discount_price}}
+                                                <span>${{$product->selling_price}}</span>
+                                            @endif
+                                        </div>
+                                        <div class="home-product-item__origin">
+                                            <span class="home-product-item__branch">{{$product->view}} view</span>
+                                            <span class="home-product-item__origin-name">{{$product->sold}} Sold</span>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="home-product-item__origin">
-                                <span class="home-product-item__branch">{{$product->view}} view</span>
-                                <span class="home-product-item__origin-name">{{$product->sold}} Sold</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-             @endforeach
-        </div>
-    @endforeach
-    @endif
-    </div>
+                        @endforeach
 
-    <!-- policy-->
-    <div class="container">
-        <div class="characteristics">
-            <div class="container">
-                <div class="row">
-
-                    <!-- Char. Item -->
-                    <div class="col-lg-3 col-md-6 char_col">
-
-                        <div class="char_item d-flex flex-row align-items-center justify-content-start">
-                            <div class="char_icon"><img src="{{asset('frontend/images/char_1.png')}}" alt=""></div>
-                            <div class="char_content">
-                                <div class="char_title">Free Delivery</div>
-                                <div class="char_subtitle">from $50</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Char. Item -->
-                    <div class="col-lg-3 col-md-6 char_col">
-
-                        <div class="char_item d-flex flex-row align-items-center justify-content-start">
-                            <div class="char_icon"><img src="{{asset('frontend/images/char_2.png')}}" alt=""></div>
-                            <div class="char_content">
-                                <div class="char_title">Free Delivery</div>
-                                <div class="char_subtitle">from $50</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Char. Item -->
-                    <div class="col-lg-3 col-md-6 char_col">
-
-                        <div class="char_item d-flex flex-row align-items-center justify-content-start">
-                            <div class="char_icon"><img src="{{asset('frontend/images/char_3.png')}}" alt=""></div>
-                            <div class="char_content">
-                                <div class="char_title">Free Delivery</div>
-                                <div class="char_subtitle">from $50</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Char. Item -->
-                    <div class="col-lg-3 col-md-6 char_col">
-
-                        <div class="char_item d-flex flex-row align-items-center justify-content-start">
-                            <div class="char_icon"><img src="{{asset('frontend/images/char_4.png')}}" alt=""></div>
-                            <div class="char_content">
-                                <div class="char_title">Free Delivery</div>
-                                <div class="char_subtitle">from $50</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
+                @endif
             </div>
-        </div>
 
-    </div>
+            <!-- policy-->
+            <div class="container">
+                <div class="characteristics">
+                    <div class="container">
+                        <div class="row">
+
+                            <!-- Char. Item -->
+                            <div class="col-lg-3 col-md-6 char_col">
+
+                                <div class="char_item d-flex flex-row align-items-center justify-content-start">
+                                    <div class="char_icon"><img src="{{asset('frontend/images/char_1.png')}}" alt="">
+                                    </div>
+                                    <div class="char_content">
+                                        <div class="char_title">Free Delivery</div>
+                                        <div class="char_subtitle">from $50</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Char. Item -->
+                            <div class="col-lg-3 col-md-6 char_col">
+
+                                <div class="char_item d-flex flex-row align-items-center justify-content-start">
+                                    <div class="char_icon"><img src="{{asset('frontend/images/char_2.png')}}" alt="">
+                                    </div>
+                                    <div class="char_content">
+                                        <div class="char_title">Free Delivery</div>
+                                        <div class="char_subtitle">from $50</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Char. Item -->
+                            <div class="col-lg-3 col-md-6 char_col">
+
+                                <div class="char_item d-flex flex-row align-items-center justify-content-start">
+                                    <div class="char_icon"><img src="{{asset('frontend/images/char_3.png')}}" alt="">
+                                    </div>
+                                    <div class="char_content">
+                                        <div class="char_title">Free Delivery</div>
+                                        <div class="char_subtitle">from $50</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Char. Item -->
+                            <div class="col-lg-3 col-md-6 char_col">
+
+                                <div class="char_item d-flex flex-row align-items-center justify-content-start">
+                                    <div class="char_icon"><img src="{{asset('frontend/images/char_4.png')}}" alt="">
+                                    </div>
+                                    <div class="char_content">
+                                        <div class="char_title">Free Delivery</div>
+                                        <div class="char_subtitle">from $50</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
 
 @endsection
